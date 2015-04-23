e8f196a9db1f9e1d103fc5bbf963d9458b9b568d
Bahout <nicolas.bahout@pigdata.net>
2015-04-23 15:53:54 +0200

    correction dist and disk size

--- scripts/azure_create_vms.sh
+++ scripts/azure_create_vms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-vmimage="b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-14_04_1-LTS-amd64-server-20150123-en-us-30GB" #azure vm image list #azure vm image show $vmimage
+vmimage="b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-14_04_2_LTS-amd64-server-20150309-en-us-30GB" #azure vm image list #azure vm image show $vmimage
 vmusername="azureuser"
-vmsize="Standard_D1"
+vmsize="Standard_D13"
 vmsshcert="mdoa.pem"
 vmlocation="North Europe"
 vmavailabilityset="mongoDB"
@@ -26,7 +26,7 @@ do
 	databloburl=https://$storageaccountname.blob.core.windows.net/vhds/${vmname}_data1.vhd
 
 	azure vm create --connect --vm-name $vmname --vm-size $vmsize --ssh $vmsshport --virtual-network-name "$vmvnetnet" --availability-set $vmavailabilityset --blob-url $osbloburl --static-ip $vmipaddress --no-ssh-password --ssh-cert $vmsshcert $vmservice $vmimage $vmusername
-	azure vm disk attach-new $vmname 50 $databloburl
+	azure vm disk attach-new $vmname 1000 $databloburl
 
 	if [ "$?" != "0" ]; then
         echo "Error: provisioning VM ${azurevmname} failed"
